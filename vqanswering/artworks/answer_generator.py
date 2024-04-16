3be58b0ce68be4ec5cf8fc9255bb7a5c9250adc8
Filippo Principi <arkfil@gmail.com>
2024-04-16 11:42:44 +0200

    Added 1 artwork

--- vqanswering/artworks/answer_generator.py
+++ vqanswering/artworks/answer_generator.py
@@ -57,16 +57,7 @@ class AnswerGenerator:
         #     # "Remember to answer in the same language as the question. "
         #     "It's very important that the answer is in the same language of the question "
         # )
-        system_prompt = (
-            "I will act as an art and language expert and respond in the same language as your question as concisely as possible within 30 words. "
-            "I will translate the answer in the same language as your question."
-            "I'll keep in mind the following guidelines: "
-            "- If your question is unrelated to artwork, I will let you know. "
-            "- If I don't have the information you're asking for, I'll indicate that by saying 'I don't have this information' in the language of your question. "
-            "- If I find it difficult to understand your question, I'll ask you to clarify. "
-            "- I won't start my response with 'Answer:' and will only use information available in our conversation. "
-            "- If I can't answer based on our conversation, I may provide an answer based on my internal knowledge, but it could be imprecise."
-            "- If the answer is longer than 30 words, I'll rephrase it to stay within the limit. ")
+
         system_prompt = (
             "Follow these steps to answer the user question: "
             "Step 1: Read the question carefully, understand it and remember the language it has been written. "
@@ -78,9 +69,7 @@ class AnswerGenerator:
             "- If there's difficulty understanding the question, ask the user to clarify the question. "
             "- Never start your answer with 'Answer:' and never use names or information that are not in the 'Context'. "
             "- If the question is in first person singular, respond in second person singular. "
-            # "- If the answer is not in the same language as the question, translate it. "
             "Step 3: Translate the produced answer in the same language of the question. "
-            # "Step 4: Write in a json format the question, the question language, the answer and the translated answer. "
             "Step 4: Provide the answer to the user in json format with the following keys: question, question language, answer, answer translated. "
         )
 
@@ -126,22 +115,7 @@ class AnswerGenerator:
             retry_count += 1
             print("Retrying in {} second(s)...".format(retry_delay))
             time.sleep(retry_delay)
-        # prompt_translate = (f"Consider the question '{question}' and the provided answer '{answer} "
-        #                     f"Question: 'Translate the answer in the language of the question. \n"
-        #                     f"Answer:")
-        # system_prompt_translate = (
-        #     "I will act as an art expert and able language translator. "
-        #     "I will translate the answer in the same language as your question. "
-        # )
-        # translation = openai.ChatCompletion.create(
-        #     model=model_engine,
-        #     messages=[
-        #         {"role": "system", "content": system_prompt_translate},
-        #         {"role": "user", "content": prompt_translate},
-        #     ],
-        #     temperature=0.25,
-        # )
-        # answer_translated = translation.choices[0].message["content"]
+
         answer_dic = json.loads(answer)
         print('answer', answer)
         print(answer_dic['answer'])
